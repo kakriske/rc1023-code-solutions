a1a40b74a738d5f118ddb528aaef5d7588f500c9
Kyle Kriske <kralex8.617@gmail.com>
2023-12-29 01:22:42 +0000

    Completed express-delete

---
+++ express-delete/server.ts
@@ -0,0 +1,54 @@
+import express from 'express';
+
+const app = express();
+
+type Grade = {
+  id: number;
+  name: string;
+  course: string;
+  score: number;
+};
+
+const grades: Record<number, Grade> = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95,
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100,
+  },
+  273: {
+    id: 273,
+    name: 'Håkon Wium Lie',
+    course: 'CSS',
+    score: 92,
+  },
+};
+
+app.get('/api/grades', (req, res) => {
+  const gradesArray: Grade[] = [];
+  for (const grade in grades) {
+    gradesArray.push(grades[grade]);
+  }
+  res.json(gradesArray);
+});
+
+app.delete('/api/grades/:id', (req, res) => {
+  console.log('delete endpoint hit');
+  const id = Number(req.params.id);
+  if (!grades[id]) {
+    res.sendStatus(404);
+  } else {
+    delete grades[id];
+    res.sendStatus(204);
+  }
+});
+
+app.listen(8080, () => {
+  console.log('Running on port 8080');
+});
