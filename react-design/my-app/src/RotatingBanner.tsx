a7c444acf4b519fe1c588a081b3eb02d3a097dc8
Kyle Kriske <kralex8.617@gmail.com>
2024-01-05 22:17:20 +0000

    Completed react-design --no-verify

--- react-design/my-app/src/RotatingBanner.tsx
+++ react-design/my-app/src/RotatingBanner.tsx
@@ -1,51 +1,87 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 interface RotatingBannerProps {
   items: string[];
 }
 
+interface Props {
+  word: string;
+}
 
 export function RotatingBanner({ items }: RotatingBannerProps) {
   const [currentIndex, setCurrentIndex] = useState(4);
 
+  function handleNextClick(): void {
+    const nextIndex = (currentIndex + 1) % items.length;
+    setCurrentIndex(nextIndex);
+  }
+
+  function handlePreClick(): void {
+    const nextIndex = (currentIndex - 1 + items.length) % items.length;
+    setCurrentIndex(nextIndex);
+  }
+
+  function handleSelect(index: number): void {
+    setCurrentIndex(index);
+  }
+
   return (
     <>
       <Banner word={items[currentIndex]} />
-      <div>
-        <Prev />
+      <div className="prevNextContainer">
+        <Prev onClick={handlePreClick} />
       </div>
-      <Indicators currentIndex= {currentIndex} items={items} />
+      <Indicators
+        onClick={handleSelect}
+        currentIndex={currentIndex}
+        items={items}
+      />
       <div>
-        <Next />
+        <Next onClick={handleNextClick} />
       </div>
     </>
   );
 }
 
 function Banner({ word }: Props) {
-  return <h1>{word}</h1>;
+  return <h1 className="banner">{word}</h1>;
 }
 
-function Prev() {
-  return <button>Prev</button>;
+function Prev({ onClick }) {
+  return (
+    <button className="prevNextButton" onClick={onClick}>
+      Prev
+    </button>
+  );
 }
 
-function Next() {
-  return <button>Next</button>;
+function Next({ onClick }) {
+  return (
+    <button className="prevNextButton" onClick={onClick}>
+      Next
+    </button>
+  );
 }
 
 interface IndicatorsProps {
   items: Props['items'];
   currentIndex: number;
+  onClick: (index: number) => void;
 }
 
-function Indicators({ items, currentIndex }: IndicatorsProps) {
+function Indicators({ items, currentIndex, onClick }: IndicatorsProps) {
   console.log('items', items);
   return (
-    <>
+    <div className="indicatorsContainer">
       {items.map((item, index) => (
-        <button style={{backgroundColor: currentIndex === index ? 'lightblue' : ''}}>{index}</button>
+        <button
+          onClick={() => onClick(index)}
+          style={{
+            backgroundColor: currentIndex === index ? 'lightblue' : '',
+          }}>
+          {index}
+        </button>
       ))}
-    </>
+    </div>
   );
 }
